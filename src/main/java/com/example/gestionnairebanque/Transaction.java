aac5a7aa4372529c8a4560a2786d6519d12a44e5
Tanou81 <55876186+Tanou81@users.noreply.github.com>
2023-03-31 14:15:59 +0200

    Initalisation TransactionTest et transactio

---
+++ src/main/java/com/example/gestionnairebanque/Transaction.java
@@ -0,0 +1,62 @@
+package com.example.gestionnairebanque;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+public class Transaction implements Serializable {
+    private String nom;
+    private Character c;
+    private double montant;
+
+    public Transaction() {
+    }
+
+    public Transaction(String nom, Character c, double montant) {
+        this.nom = nom;
+        this.c = c;
+        this.montant = montant;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+
+    public double getmontant() {
+        return montant;
+    }
+
+    public void setmontant(double montant) {
+        this.montant = montant;
+    }
+
+    @Override
+    public String toString() {
+        String result = "";
+        result += " nom: " + nom;
+        result += " c: " + c;
+        result += " montant: " + montant;
+        return result;
+    }
+    public Character getC() {
+        return c;
+    }
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Transaction that = (Transaction) o;
+        return Double.compare(that.montant, montant) == 0 &&
+                Objects.equals(nom, that.nom) &&
+                Objects.equals(c, that.c);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(nom, c, montant);
+    }
+}
--- src/main/java/com/example/gestionnairebanque/tests/TransactionTest.java
+++ src/main/java/com/example/gestionnairebanque/tests/TransactionTest.java
@@ -2,15 +2,92 @@ package com.example.gestionnairebanque.tests;
 import com.example.gestionnairebanque.Transaction;
 import org.junit.Test;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNull;
+import static org.junit.Assert.*;
 
 public class TransactionTest {
 
     @Test
-    public void testTransactionNull () {
+    public void testTransaction1 () {
 
         Transaction transaction = new Transaction();
-        assertNull( transaction);
+        assertEquals(transaction,  transaction);
+    }
+
+    @Test
+    public void testGetNom() {
+        Transaction transaction = new Transaction("client1", 'C', 1000.0);
+        assertEquals("client1", transaction.getNom());
+    }
+
+    @Test
+    public void testSetNom() {
+        Transaction transaction = new Transaction("client1", 'C', 1000.0);
+        transaction.setNom("client2");
+        assertEquals("client2", transaction.getNom());
+    }
+
+    @Test
+    public void testGetMontant() {
+        Transaction transaction = new Transaction("client1", 'C', 1000.0);
+        assertEquals(1000.0, transaction.getmontant(), 0.01);
+    }
+
+    @Test
+    public void testSetMontant() {
+        Transaction transaction = new Transaction("client1", 'C', 1000.0);
+        transaction.setmontant(2000.0);
+        assertEquals(2000.0, transaction.getmontant(), 0.01);
+    }
+
+    @Test
+    public void testToString() {
+        Transaction transaction = new Transaction("client1", 'C', 1000.0);
+        String expectedString = " nom: client1 c: C montant: 1000.0";
+        assertEquals(expectedString, transaction.toString());
+    }
+
+    @Test
+    public void testConstructor() {
+        Transaction t = new Transaction("John", 'D', 100.0);
+        assertNotNull(t);
+        assertEquals("John", t.getNom());
+        Character c = 'D';
+        assertEquals('D', c.charValue());
+        assertEquals(100.0, t.getmontant(), 0.001);
+    }
+
+    @Test
+    public void testSetNomBis() {
+        Transaction t = new Transaction();
+        t.setNom("Jane");
+        assertEquals("Jane", t.getNom());
+    }
+
+    @Test
+    public void testSetMontantBis() {
+        Transaction t = new Transaction();
+        t.setmontant(50.0);
+        assertEquals(50.0, t.getmontant(), 0.001);
+    }
+
+    @Test
+    public void testEquals() {
+        Transaction t1 = new Transaction("John", 'D', 100.0);
+        Transaction t2 = new Transaction("John", 'D', 100.0);
+        assertEquals(t1, t2);
+    }
+
+    @Test
+    public void testNotEquals() {
+        Transaction t1 = new Transaction("John", 'D', 100.0);
+        Transaction t2 = new Transaction("Jane", 'D', 100.0);
+        assertNotEquals(t1, t2);
+    }
+
+    @Test
+    public void testToStringBis() {
+        Transaction t = new Transaction("John", 'D', 100.0);
+        String expected = " nom: John c: D montant: 100.0";
+        assertEquals(expected, t.toString());
     }
 }
