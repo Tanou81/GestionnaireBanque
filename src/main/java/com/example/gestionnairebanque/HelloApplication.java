f8eb8a7eefe070407121b41fa067e2cec03aed45
Tanou81 <55876186+Tanou81@users.noreply.github.com>
2023-03-31 13:43:07 +0200

    Initalisation

--- src/main/java/com/example/gestionnairebanque/HelloApplication.java
+++ src/main/java/com/example/gestionnairebanque/HelloApplication.java
@@ -3,6 +3,8 @@ package com.example.gestionnairebanque;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -10,9 +12,13 @@ import java.io.IOException;
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("IHM.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 600, 200);
+
+        stage.getIcons().add(new Image(getClass().getResourceAsStream("/images/3IL.jpg")));
+        stage.setResizable(false);
+        scene.setFill(Color.LIGHTGRAY);
+        stage.setTitle("Gestion Banquaire!");
         stage.setScene(scene);
         stage.show();
     }
